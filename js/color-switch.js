67c6e962aa46ba16cf5e76682e420479bc0c36bf
Ivan Bulavenko <bulavenkoivan@gmail.com>
2021-05-12 18:02:58 +0300

    pret

--- js/color-switch.js
+++ js/color-switch.js
@@ -1,37 +1,36 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
 ];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const btn = document.querySelectorAll('button')
+const btn = document.querySelectorAll("button");
 const start = btn[0];
 const stop = btn[1];
 
-
 function changeBgColor() {
-  document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+  document.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
 }
 
 let intervalId = null;
 
 function activationStart() {
   intervalId = setInterval(changeBgColor, 1000);
-  start.disabled = 'true';
+  start.disabled = "true";
 }
 
 function clickStop() {
   clearInterval(intervalId);
-  start.disabled = '';
+  start.disabled = "";
 }
 
-
-start.addEventListener('click', activationStart);
-stop.addEventListener('click', clickStop);
+start.addEventListener("click", activationStart);
+stop.addEventListener("click", clickStop);
